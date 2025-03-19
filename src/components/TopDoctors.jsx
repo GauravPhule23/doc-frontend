23bd3c309abc4bce004fa858d401f7fb30df60ba
Shubham <realitymaniac7@gmail.com>
2025-03-20 01:56:21 +0530

    top doctors and more button

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { doctors } from '../assets/assets'
-import { Link } from 'react-router'
+import { Link, Navigate, useNavigate } from 'react-router'
 
 export const TopDoctors = () => {
+
+    const navigate = useNavigate()
+
   return (
     <div className='flex flex-col items-center py-16 gap-4 text-gray-800'>
 
@@ -16,14 +19,21 @@ export const TopDoctors = () => {
         {/* Doctor's cards */}
 
         <div className='flex flex-wrap justify-center mt-10 w-auto h-auto gap-4'>
+
             {doctors.slice(0, 10).map((val, i) => (
-                <Link to={`/appointments/${val._id}`} className='flex flex-col items-center px-1.5 hover:translate-y-[-10px] transition-all duration-500 rounded-md gap-4' key={i}>
-                    <img src={val.image} alt="" className='w-16 sm:w-52 mb-2 bg-indigo-100'/>
-                    <p className='text-xl font-medium'>{val.name}</p>
-                    <p className='font-medium'>{val.speciality}</p>
+                <Link to={`/appointments/${val._id}`} className='flex flex-col mx-1.5 hover:translate-y-[-10px] transition-all duration-500 rounded-xl border-1 border-indigo-400' key={i}>
+                    <img src={val.image} alt="" className='w-16 sm:w-52 bg-indigo-100 rounded-t-lg'/>
+                    <div className='bg-gray-100 rounded-b-xl pl-4 py-3'> 
+                        <p className='text-lg font-medium'>{val.name}</p>
+                        <p className='font-medium'>{val.speciality}</p>
+                    </div>
                 </Link>
             ))}
+
         </div>
+
+        <button onClick={() => { navigate('/doctors'); scrollTo(0,0) }} className="bg-blue-100 transition delay-100 ease-in-out hover:bg-indigo-600 text-gray-800 hover:text-white px-12 py-3 rounded-full mt-10 cursor-pointer" > more </button>
+
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Header } from '../components/Header'
 import { SpecialityMenu } from '../components/SpecialityMenu'
 import { TopDoctors } from '../components/TopDoctors'
+import { Banner } from '../components/Banner'
 
 
 export const Home = () => {
@@ -10,6 +11,7 @@ export const Home = () => {
       <Header></Header>
       <SpecialityMenu></SpecialityMenu>
       <TopDoctors></TopDoctors>
+      <Banner></Banner>
     </div>
   )
 }
