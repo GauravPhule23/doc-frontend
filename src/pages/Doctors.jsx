6c54624328f18f7a66f248ae2f006d97c5e3360c
Gaurav Phule <gaurav230605@gmail.com>
2025-04-11 20:48:59 +0530

    Update Doctors.jsx
    uses the static data if it does not get data from the backend

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
-import { useParams } from 'react-router'
-import { Link } from 'react-router';
-import { useContext } from 'react'
-import { useState } from 'react'
-import { doctors } from '../assets/assets'
-// import { Appcontext } from '../context/Appcontext'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { doctors as staticDoctors } from '../assets/assets';
 
 export const Doctors = () => {
+  const [doctorsList, setDoctorsList] = useState([]);
 
-  // const { doctors } = useContext(Appcontext);
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        const res = await axios.get('https://quickcare-backend.vercel.app/api/doctors');
+        if (res.data && res.data.data && res.data.data.length > 0) {
+          setDoctorsList(res.data.data);
+        } else {
+          setDoctorsList(staticDoctors);
+        }
+      } catch (error) {
+        console.error('Failed to fetch doctors from backend, using static data.', error);
+        setDoctorsList(staticDoctors);
+      }
+    };
+
+    fetchDoctors();
+  }, []);
 
   return (
     <div>
-
       <div className='flex flex-wrap justify-center mt-10 w-auto h-auto gap-4'>
-
-        {doctors.map((val, i) => (
-            <Link to={`/appointments/${val._id}`} className='flex flex-col mx-1.5 hover:translate-y-[-10px] transition-all duration-500 rounded-xl border-1 border-indigo-400' key={i}>
-                <img src={val.image} alt="" className='w-16 sm:w-52 bg-indigo-100 rounded-t-lg'/>
-                <div className='bg-gray-100 rounded-b-xl pl-4 py-3'> 
-                    <p className='text-lg font-medium'>{val.name}</p>
-                    <p className='font-medium'>{val.speciality}</p>
-                </div>
-            </Link>
+        {doctorsList.map((val, i) => (
+          <Link
+            to={`/appointments/${val._id}`}
+            className='flex flex-col mx-1.5 hover:translate-y-[-10px] transition-all duration-500 rounded-xl border-1 border-indigo-400'
+            key={i}
+          >
+            <img src={val.image} alt="" className='w-16 sm:w-52 bg-indigo-100 rounded-t-lg' />
+            <div className='bg-gray-100 rounded-b-xl pl-4 py-3'>
+              <p className='text-lg font-medium'>{val.name}</p>
+              <p className='font-medium'>{val.speciality}</p>
+            </div>
+          </Link>
         ))}
-
       </div>
-
     </div>
-
-  )
-}
+  );
+};
